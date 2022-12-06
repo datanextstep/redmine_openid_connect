d2b9ea8fa6dded84928e105cdeb327d5607974d2
Tristan <starcraft66@gmail.com>
2022-12-06 15:46:14 -0500

    Update plugin description for fork

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_openid_connect do
   author 'Alfonso Juan Dillera / Markus M. May'
   description 'OpenID Connect implementation for Redmine'
   version '0.9.6'
-  url 'https://github.com/eea/redmine_openid_connect'
+  url 'https://github.com/datanextstep/redmine_openid_connect'
   author_url 'http://github.com/adillera'
 
   settings :default => { 'empty' => true }, partial: 'settings/redmine_openid_connect_settings'
